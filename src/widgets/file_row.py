f2cefd6aeb9a30650c97b59dd4d2db44d0a33159
Nokse22 <44558032+Nokse22@users.noreply.github.com>
2024-06-11 13:48:46 +0200

    added drop target to file row

--- src/widgets/file_row.py
+++ src/widgets/file_row.py
@@ -30,11 +30,13 @@ class FileRow(Adw.ActionRow):
     __gsignals__ = {
         'open-file': (GObject.SignalFlags.RUN_FIRST, None, ()),
         'delete-file': (GObject.SignalFlags.RUN_FIRST, None, ()),
+        'file-added': (GObject.SignalFlags.RUN_FIRST, None, (str,)),
     }
 
     file_button = Gtk.Template.Child()
     filename_label = Gtk.Template.Child()
     delete_button = Gtk.Template.Child()
+    drop_target = Gtk.Template.Child()
 
     filepath = ""
 
@@ -43,6 +45,9 @@ class FileRow(Adw.ActionRow):
 
         self.file_button.connect("clicked", self.on_open_clicked)
         self.delete_button.connect("clicked", self.on_delete_clicked)
+        self.drop_target.connect("drop", self.on_drop_received)
+
+        self.drop_target.set_gtypes([Gdk.FileList])
 
     def on_open_clicked(self, btn):
         self.emit("open-file")
@@ -60,3 +65,8 @@ class FileRow(Adw.ActionRow):
         self.filename_label.set_visible(False)
         self.delete_button.set_visible(False)
         self.emit("delete-file")
+
+    def on_drop_received(self, drop, value, x, y):
+        filepath = value.get_files()[0].get_path()
+        self.emit("file-added", filepath)
+        self.set_filename(filepath)
