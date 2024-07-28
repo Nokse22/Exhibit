8bcda2c045b1bbdfdf87fd11ba87a10dd50742c3
Nokse22 <44558032+Nokse22@users.noreply.github.com>
2024-07-28 22:53:06 +0200

    increased default light intensity

--- src/settings_manager.py
+++ src/settings_manager.py
@@ -83,7 +83,7 @@ class WindowSettings(Gio.ListStore):
         "ambient-occlusion": False,
         "anti-aliasing": True,
         "hdri-ambient": False,
-        "light-intensity": 1.0,
+        "light-intensity": 3.0,
 
         "show-edges": False,
         "edges-width": 1.0,
